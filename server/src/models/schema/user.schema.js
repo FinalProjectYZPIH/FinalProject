ff0259db1ce843f5f256ab0ac828b3da351df002
Yanhui Wu <yanmac@YanMac.fritz.box>
2023-10-04 20:45:36 +0200

    server: Added: Usermodel and ZodValidierung

--- server/src/models/schema/user.schema.js
+++ server/src/models/schema/user.schema.js
@@ -1,1 +1,172 @@
+import zod from "zod";
+
 // Schema zur Validierung des Datens
+
+// Benutzerdefinierte Validierungsfunktion für den Datenbank mit Regex
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+export const validateEmail = (email) => {
+  return emailRegex.test(email);
+};
+
+export const validatePassword = (password) => {
+  return passwordRegex.test(password);
+};
+
+//ZodValidierung
+
+export const registerFormSchema = z.object({
+  body: z // Für die Registrungsvalidation Achtung >>  Achte auf body key vom request
+    .object({
+      firstname: z
+        .string()
+        .min(2, { message: "minimum 2 Characters" })
+        .max(15, { message: "maximun 15 Characters" })
+        .trim()
+        .toLowerCase()
+        .refine((value) => {
+          if (!value) throw { message: "name required" };
+          return true;
+        }),
+      lastname: z
+        .string()
+        .min(2, { message: "minimum 2 Characters" })
+        .max(15, { message: "maximun 15 Characters" })
+        .trim()
+        .toLowerCase()
+        .refine((value) => {
+          if (!value) throw { message: "name required" };
+          return true;
+        }),
+      nickname: z
+        .string()
+        .min(2, { message: "minimum 2 Characters" })
+        .max(15, { message: "maximun 15 Characters" })
+        .trim()
+        .toLowerCase()
+        .optional(),
+      dateTime: z
+        .string()
+        .dateTime({ message: "Invalid datetime string! Must be UTC." }), // genaue Info >>https://zod.dev/?id=basic-usage  datetime Key eingeben
+      email: z
+        .string()
+        .trim()
+        .toLowerCase()
+        .email()
+        .refine((value) => {
+          if (!value) throw { message: "email required" };
+
+          return validateEmail(value);
+        }),
+      emailConfirmation: z.string({
+        required_error: "passwordConfirmation is required",
+      }),
+      password: z
+        .string()
+        .min(8, { message: "minimun 8 Characters" })
+        .trim()
+        .refine((value) => {
+          if (!value) throw { message: "password required" };
+          return validatePassword(value);
+        }),
+      passwordConfirmation: z.string({
+        required_error: "passwordConfirmation is required",
+      }),
+    })
+    .refine((data) => data.password === data.passwordConfirmation, {
+      message: "Passwords do not match",
+      path: ["passwordConfirmation"],
+    })
+    .refine((data) => data.email === data.emailConfirmation, {
+      message: "Emails do not match",
+      path: ["emailConfirmation"],
+    }),
+});
+
+
+export const emailLoginSchema = z.object({
+  email: z
+  .string()
+  .trim()
+  .toLowerCase()
+  .email()
+  .refine((value) => {
+    if (!value) throw { message: "email required" };
+    return validateEmail(value);
+  }),
+  password: z
+  .string()
+  .min(8, { message: "minimun 8 Characters" })
+  .trim()
+  .refine((value) => {
+    if (!value) throw { message: "password required" };
+    return validatePassword(value);
+  }),
+});
+
+export const nameLoginSchema = z.object({
+  nickname: z
+  .string()
+  .min(2, { message: "minimum 2 Characters" })
+  .max(15, { message: "maximun 15 Characters" })
+  .trim()
+  .toLowerCase()
+  .refine((value) => {
+    if (!value) throw { message: "name required" };
+    return true;
+  }),
+  password: z
+  .string()
+  .min(8, { message: "minimun 8 Characters" })
+  .trim()
+  .refine((value) => {
+    if (!value) throw { message: "password required" };
+    return validatePassword(value);
+  }),
+});
+
+
+export const mixLoginSchema = nameLoginSchema || emailLoginSchema;
+
+
+
+
+// const loginFieldEnum = z.enum(["nickname", "email"]); // enum heißt   der value kann nur einen der beiden value sein
+
+// export const loginFormSchema = z.object({     //  vorerst nur diese lösung   noch nicht ausprobiert
+//   field: loginFieldEnum,
+//   value: z.string().refine((value, data) => {
+//     const { field } = data;
+//     if (field === "nickname") {
+//       return value
+//         .min(2, { message: "minimum 2 Characters" })
+//         .max(15, { message: "maximum 15 Characters" })
+//         .trim()
+//         .toLowerCase()
+//         .refine((value) => {
+//           if (!value) throw { message: "name required" };
+//           return true;
+//         });
+//     }
+//     if (field === "email") {
+//       return value
+//         .trim()
+//         .toLowerCase()
+//         .email()
+//         .refine((value) => {
+//           if (!value) throw { message: "email required" };
+//           return true;
+//         });
+//     }
+//     return value;
+//   }),
+//   password: z
+//     .string()
+//     .min(8, { message: "minimum 8 Characters" })
+//     .trim()
+//     .refine((value) => {
+//       if (!value) throw { message: "password required" };
+//       return validatePassword(value);
+//     }),
+// });
--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,1 +1,49 @@
 // model fürs Datenbank >> MongdoDB
+
+import mongoose from "mongoose";
+import { validateEmail, validatePassword } from "./schema/user.schema";
+
+const userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      min: [2, "Mindestens 2 Buchstaben"],
+      match: [/^[A-Za-z]+$/, "Nur Buchstaben erlaubt"],
+    },
+    lastname: {
+      type: String,
+      min: [2, "Mindestens 2 Buchstaben"],
+      match: [/^[A-Za-z]+$/, "Nur Buchstaben erlaubt"],
+    },
+    nickname: {
+      type: String,
+      min: [2, "Mindestens 2 Buchstaben"],
+      match: [/^[A-Za-z0-9]+$/, "Kein Sonderzeichen erlaubt"],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      validate: [validateEmail, "Ungültige Email-Adresse"],
+    },
+    role: {
+      type: String,
+      enum: {
+        values: ["member", "admin"],
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate: [
+        validatePassword,
+        "Mindestens 8 Zeichen lang \n Mindestens ein Kleinbuchstabe \n Mindestens ein Großbuchstabe \n Mindestens eine Ziffer \n Mindestens ein Sonderzeichen (z. B. @$!%*?&)",
+      ],
+    },
+    avatarImage: String,
+    birthday: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
