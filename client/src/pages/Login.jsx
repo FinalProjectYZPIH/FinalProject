89d8831af691aea1989677ddfc28155ad7861066
zoerix <zoe.rix.z@gmail.com>
2023-10-17 15:47:23 +0200

    some updates at the signup but styling not finished!

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,17 +1,12 @@
 
 import {LoginComponent} from '../components/LoginComponent.jsx';
-import { ButtonGoogle } from '../components/Buttons.jsx';
-import { ButtonLogin } from '../components/Buttons.jsx';
+
 
 
 const Login = () => {
   return (
     <>
-     
       <LoginComponent />
- 
-     
-
     </>
   )
 }
