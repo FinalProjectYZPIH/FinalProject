b69a3c3498609bf13dc6fbf74e885b976f5de2c7
zoerix <zoe.rix.z@gmail.com>
2023-10-18 11:45:42 +0200

    signUp elements more in place

--- client/src/components/LoginComponent.jsx
+++ client/src/components/LoginComponent.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { ButtonGoogle } from '../components/Buttons.jsx';
 import { ButtonLogin } from '../components/Buttons.jsx';
 import { ButtonSignIn } from '../components/Buttons.jsx';
-
+import { Inputs } from './Inputs.jsx';
 
 export const LoginComponent = () => {
     return (
@@ -12,19 +12,12 @@ export const LoginComponent = () => {
                 <h1 className="text-4xl text-white-bold text-center mb-6">LOGIN</h1>
                 <ButtonGoogle />
                 <form action="" method="post">
-                    <div className="relative my-4">
-                        <input type="email" className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-grey-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600" />
-                        <label htmlFor="email" className="absolute text-sm text-white bg-transparent duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75">Your Email</label>
-                    </div>
-
-                    <div className="relative my-4">
-                        <input type="password" className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-grey-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600" />
-                        <label htmlFor="password" className="absolute text-sm text-white bg-transparent duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75">Your Password</label>
-                    </div>
+                    <Inputs ph="Your Email">your email</Inputs>
+                    <Inputs ph="Your Password">your password</Inputs>
                     <div>
                         <div>
                             <input type="checkbox" name="checkbox" id="checkbox" />
-                            <label htmlFor="">Remember Me</label>
+                            <label htmlFor="checkbox">Remember Me</label>
                         </div>
                         <ButtonLogin />
                         <span>Forgot Password</span>
@@ -33,7 +26,7 @@ export const LoginComponent = () => {
                         <span>New here? <Link to='/Signup'><ButtonSignIn /></Link> </span>
                     </div>
                 </form>
-            </div >
-        </div >
-    )
-}
+            </div>
+        </div>
+    );
+};
--- client/src/components/SignUpComponent.jsx
+++ client/src/components/SignUpComponent.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { ButtonSignIn } from './Buttons'
-import { Inputs } from './Inputs'
+import React from 'react';
+import { ButtonSignIn } from './Buttons';
+import { Inputs } from './Inputs';
 
 export const SignUpComponent = () => {
     return (
@@ -8,17 +8,17 @@ export const SignUpComponent = () => {
             <div className="p-10 m-10 h-screen w-screen-sm bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
                 <h1 className="text-4xl text-white-bold text-center mb-6">SIGN UP</h1>
                 <form action="">
-                    <Inputs ph="firstname">firstname</Inputs>
-                    <Inputs ph="lastname">lastname</Inputs>
-                    <Inputs ph="username">username</Inputs>
-                    <Inputs ph="email">email</Inputs>
-                    <Inputs ph="confirm email">confirm email</Inputs>
-                    <Inputs ph="birthday">birthday</Inputs>
-                    <Inputs ph="password">password</Inputs>
-                    <Inputs ph="confirm password">confirm password</Inputs>
+                    <Inputs label="First Name" ph="Enter your first name" />
+                    <Inputs label="Last Name" ph="Enter your last name" />
+                    <Inputs label="Username" ph="Choose a username" />
+                    <Inputs label="Email" ph="Enter your email" />
+                    <Inputs label="Confirm Email" ph="Confirm your email" />
+                    <Inputs label="Birthday" ph="Enter your birthday" />
+                    <Inputs label="Password" ph="Choose a password" />
+                    <Inputs label="Confirm Password" ph="Confirm your password" />
                     <ButtonSignIn />
                 </form>
             </div>
         </div>
-    )
-}
+    );
+};
