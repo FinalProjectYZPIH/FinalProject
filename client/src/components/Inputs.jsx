437f88b5d9d8915aae694076f1ce542b8b6e60be
zoerix <zoe.rix.z@gmail.com>
2023-10-19 12:32:10 +0200

    added images in tailwind.config

--- client/src/components/Inputs.jsx
+++ client/src/components/Inputs.jsx
@@ -3,7 +3,7 @@ export const Inputs = ({ type, label, ph, onChangeFn, value }) => {
         <div className='my-4'>
             <label htmlFor={label} className="text-sm text-white">{label}</label>
             <input
-                className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-grey-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600"
+                className="bg-blue-900 block w-72 py-2.3 px-0 text-lg text-white  border-b-2 rounded-lg border-grey-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600"
                 type={type}
                 placeholder={ph}
                 id={label}
--- client/src/components/LoginComponent.jsx
+++ client/src/components/LoginComponent.jsx
@@ -1,17 +1,24 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { ButtonGoogle } from '../components/Buttons.jsx';
-import { ButtonLogin } from '../components/Buttons.jsx';
-import { ButtonSignIn } from '../components/Buttons.jsx';
+import { Button } from './Buttons.jsx';
+import { MediaButtons } from './Buttons.jsx';
+import { GoogleIcons } from '../assets/Icons.jsx';
+import { FacebookIcons } from '../assets/Icons.jsx';
 import { Inputs } from './Inputs.jsx';
+import backgroundImages from '../../tailwind.config.js';
+
+
+
+// style={{ background: 'url("./src/assets/bg1.jpg")', backgroundPosition: 'center' }}
 
 export const LoginComponent = () => {
     return (
-        <div className='flex items-center justify-center' style={{ background: 'url("./src/assets/bg1.jpg")', backgroundPosition:'center' }}>
-            <div className="bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
+        <div className='flex items-center justify-center bg-bgLightMode bg-cover' >
+            <div className="bg-slate-800 bg-opacity-75 md:bg-opacity-25 p-10 m-10 h-screen w-screen  border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm ">
                 <h1 className="text-4xl text-white-bold text-center mb-6">LOGIN</h1>
-                <ButtonGoogle />
+                <MediaButtons> <GoogleIcons />sign in with google</MediaButtons>
+                <MediaButtons><FacebookIcons /> sign in with facebook</MediaButtons>
                 <form action="" method="post">
                     <Inputs ph="Your Email">your email</Inputs>
                     <Inputs ph="Your Password">your password</Inputs>
@@ -20,15 +27,15 @@ export const LoginComponent = () => {
                             <input type="checkbox" name="checkbox" id="checkbox" />
                             <label htmlFor="checkbox">Remember Me</label>
                         </div>
-                        <ButtonLogin />
+                        <Button>Login</Button>
                         <span>Forgot Password</span>
                     </div>
                     <div>
-                        <span>New here? <Link to='/Signup'><ButtonSignIn /></Link> </span>
+                        <span>New here? <Link to='/Signup'><Button>Sign In</Button></Link> </span>
                     </div>
                 </form>
             </div>
-        </div>
+        </div >
     );
 
 };
