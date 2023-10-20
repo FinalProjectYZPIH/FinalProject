0bdc34491b4d71531aaa9d8e3f5671d476831ff4
zoerix <zoe.rix.z@gmail.com>
2023-10-21 00:16:59 +0200

    added styling for login  & sign up

--- client/src/components/LoginComponent.jsx
+++ client/src/components/LoginComponent.jsx
@@ -12,23 +12,23 @@ export const LoginComponent = () => {
     return (
         <div className='flex items-center justify-center bg-bgLightMode bg-cover h-screen w-screen-sm bg-cover' >
             <div className="p-10 m-10 h-screen-sm w-screen-sm bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-25">
-                <h1 className="text-4xl text-white-bold text-center mb-6">LOGIN</h1>
+                <h1 className="text-4xl font-orbitron text-cyan-500 text-center mb-7 border-b-4  border-cyan-400 p-4">LOGIN</h1>
                 <MediaButtons> <GoogleIcons />sign in with google</MediaButtons>
                 <MediaButtons><FacebookIcons />sign in with facebook</MediaButtons>
                 <form action="" method="post">
 
-                    <Inputs label="enter email" ph="Your Email" type="email">your email</Inputs>
-                    <Inputs label="enter password" ph="Your Password" type="password">your password</Inputs>
+                    <Inputs label="email" ph="enter your @" type="email">email</Inputs>
+                    <Inputs label="password" ph="enter your password" type="password">password</Inputs>
                     <div>
-                        <div>
+                        <div className='text-center'>
                             <input type="checkbox" name="checkbox" id="checkbox" />
-                            <label htmlFor="checkbox">Remember Me</label>
+                            <label htmlFor="checkbox" className='text-cyan-400 p-5'>Remember Me</label>
                         </div>
                         <Button>Login</Button>
-                        <span>Forgot Password</span>
+                        <p className='text-cyan-400 p-5  text-sm text-center'>Forgot Password</p>
                     </div>
                     <div>
-                        <span>New here? <Link to='/Signup'><Button>Sign In</Button></Link> </span>
+                        <p className='text-cyan-400 p-5 text-sm text-center'>New here? <Link to='/Signup'><Button>Sign In</Button></Link> </p>
                     </div>
                 </form>
             </div>
--- client/src/components/SignUpComponent.jsx
+++ client/src/components/SignUpComponent.jsx
@@ -1,15 +1,17 @@
 
 import React from 'react';
+import '../index.css'
 // import { useNavigate } from 'react-router-dom';
 import { Button } from './Buttons'
 import { Inputs } from './Inputs';
 import backgroundImages from '../../tailwind.config.js'
 export const SignUpComponent = () => {
- 
+
     return (
-        <div className='flex items-center justify-center bg-bgLightMode bg-cover  h-screen'  >
-            <div className="p-10 m-10 h-screen-sm w-screen-sm bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-25">
-                <h1 className="text-4xl text-blue-600 text-center mb-6">SIGN UP</h1>
+        <div className='flex items-center justify-center bg-bgLightMode h-screen w-screen-sm bg-cover'  >
+            {/* <div className='bg-violet-950 bg-opacity-25 mt-8'> */}
+            <div className="p-10 m-8 mt-16 h-100 bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-25">
+                <h1 className="text-4xl font-orbitron text-cyan-500 text-center mb-7 border-b-4  border-cyan-400 p-4 mt-2">SIGN UP</h1>
                 <form action="">
                     <Inputs label="First Name" ph="Enter your first name" type="text" />
                     <Inputs label="Last Name" ph="Enter your last name" type="text" />
@@ -19,10 +21,15 @@ export const SignUpComponent = () => {
                     <Inputs label="Birthday" ph="Enter your birthday" type="date" />
                     <Inputs label="Password" ph="Choose a password" type="password" />
                     <Inputs label="Confirm Password" ph="Confirm your password" type="password" />
+                    <div className='text-center'>
+                        <input type="checkbox" name="checkbox" id="checkbox" />
+                        <label htmlFor="checkbox" className='text-cyan-400 p-5'>agree2theTermsOfUse</label>
+                    </div>
                     <Button>Sign In </Button>
                 </form>
             </div>
         </div>
+        // </div>
     );
 };
 
