ba6c780ab2b8390ea386934268ebcc8bc2514b4b
zoerix <zoe.rix.z@gmail.com>
2023-10-21 12:49:37 +0200

    login ok?!

--- client/src/components/Buttons.jsx
+++ client/src/components/Buttons.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Button = ({ children }) => {
   return (
     <div className="flex items-center justify-center">
-      <button className="flex items-center bg-transparent hover:border-y-teal-400 hover:bg-cyan-400 text-sky-400 font-semibold hover:text-white hover:bg-opacity-1 py-2 px-4 border border-cyan-400 hover:border-transparent rounded m-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-300">
+      <button className="flex items-center bg-transparent hover:border-y-teal-400 hover:bg-cyan-400 text-sky-400 font-semibold hover:text-white hover:bg-opacity-1 p-4 border border-cyan-400 hover:border-transparent rounded m-2 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-300">
         {children}
       </button>
     </div>
@@ -13,7 +13,7 @@ export const Button = ({ children }) => {
 export const MediaButtons = ({ children }) => {
   return (
     <div className="flex items-center justify-center">
-      <button className="m-2  flex items-center bg-transparent  bg-opacity-60 border border-cyan-400 rounded-lg shadow-md text-sm font-medium text-gray-800 hover:bg-cyan-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-300">
+      <button className="m-2 p-2 flex items-center bg-transparent  bg-opacity-60 border border-cyan-400 rounded-lg shadow-md text-sm font-medium text-gray-800 hover:bg-cyan-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-300">
         {children}
       </button>
     </div>
--- client/src/components/LoginComponent.jsx
+++ client/src/components/LoginComponent.jsx
@@ -12,20 +12,28 @@ export const LoginComponent = () => {
     return (
         <div className='flex items-center justify-center bg-bgLightMode bg-cover h-screen w-screen-sm bg-cover' >
             <div className="p-10 m-10 h-screen-sm w-screen-sm bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-25">
-                <h1 className="text-4xl font-orbitron text-cyan-500 text-center mb-7 border-b-4  border-cyan-400 p-4">LOGIN</h1>
-                <MediaButtons> <GoogleIcons />sign in with google</MediaButtons>
-                <MediaButtons><FacebookIcons />sign in with facebook</MediaButtons>
+                <h1 className="text-4xl font-orbitron text-cyan-500 text-center mb-11 border-b-4  border-cyan-400 p-4 ">LOGIN</h1>
+
                 <form action="" method="post">
 
                     <Inputs label="email" ph="enter your @" type="email">email</Inputs>
                     <Inputs label="password" ph="enter your password" type="password">password</Inputs>
                     <div>
+
+                        <Button>Login</Button>
+                        <p className='text-cyan-400 p-5  text-sm text-center'>Forgot Password</p>
                         <div className='text-center'>
                             <input type="checkbox" name="checkbox" id="checkbox" />
                             <label htmlFor="checkbox" className='text-cyan-400 p-5'>Remember Me</label>
+                            <p className='text-cyan-400 p-5 text-center'>or </p>
                         </div>
-                        <Button>Login</Button>
-                        <p className='text-cyan-400 p-5  text-sm text-center'>Forgot Password</p>
+
+                        <div className='h-24'>
+
+                            <MediaButtons> <GoogleIcons />sign in with google</MediaButtons>
+                            <MediaButtons><FacebookIcons />sign in with facebook</MediaButtons>
+                        </div>
+
                     </div>
                     <div>
                         <p className='text-cyan-400 p-5 text-sm text-center'>New here? <Link to='/Signup'><Button>Sign In</Button></Link> </p>
