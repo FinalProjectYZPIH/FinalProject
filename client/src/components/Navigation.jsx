536997ab0cd51393ee76a5a00ded65760a48a197
pawel-w88 <rws.pawlak@gmail.com>
2023-10-25 13:51:22 +0200

    add modus switch install react-switch and lucide-react

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,16 +1,17 @@
-import Switch from "@mui/material/Switch";
-import {StyledEngineProvider} from '@mui/material/styles';
 import { useDarkLightMode } from "../context/data/dataStore";
+import { CloudMoon, Sun } from "lucide-react";
+import ReactSwitch from "react-switch";
 
 export default function Navigation() {
   const { lightMode, setDarkMode } = useDarkLightMode();
   return (
-    <StyledEngineProvider>
-      <Switch
-        className="flex items-center bg-transparent text-blue-700 font-semibold "
-        onClick={setDarkMode}
-      >
-      </Switch>
-    </StyledEngineProvider>
+    <ReactSwitch
+      onChange={setDarkMode}
+      checked={!lightMode}
+      offColor={"#3b82f6"}
+      onColor={"#D0D0D0"}
+      checkedIcon={<CloudMoon />}
+      uncheckedIcon={<Sun />}
+    />
   );
 }
