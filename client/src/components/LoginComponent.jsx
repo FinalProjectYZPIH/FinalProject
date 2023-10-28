630b4600087ed8d5b3f1c99c11aa55c0db0dbe54
Yanhui Wu <speicher5555@gmail.com>
2023-10-28 10:43:46 +0200

    merge yanremote

--- client/src/components/LoginComponent.jsx
+++ client/src/components/LoginComponent.jsx
@@ -6,19 +6,25 @@ import { ButtonSignIn } from '../components/Buttons.jsx';
 
 
 export const LoginComponent = () => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+    }
+    
     return (
         <div className='flex items-center justify-center'>
             <div className="bg-slate-800 border border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
                 <h1 className="text-4xl text-white-bold text-center mb-6">LOGIN</h1>
                 <ButtonGoogle />
-                <form action="" method="post">
+                <form action="" method="post" onSubmit={handleSubmit}>
                     <div className="relative my-4">
-                        <input type="email" className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-grey-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600" />
+                        <input type="email" name='email' className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-grey-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600" />
                         <label htmlFor="email" className="absolute text-sm text-white bg-transparent duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75">Your Email</label>
                     </div>
 
                     <div className="relative my-4">
-                        <input type="password" className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-grey-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600" />
+                        <input type="password" name="password" className="block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-grey-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600" />
                         <label htmlFor="password" className="absolute text-sm text-white bg-transparent duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75">Your Password</label>
                     </div>
                     <div>
@@ -26,11 +32,12 @@ export const LoginComponent = () => {
                             <input type="checkbox" name="checkbox" id="checkbox" />
                             <label htmlFor="">Remember Me</label>
                         </div>
-                        <ButtonLogin />
+                        <button type='sumbit' className='border border-1 bg-red-100'>Login</button>
+                        <ButtonLogin>Login</ButtonLogin>
                         <span>Forgot Password</span>
                     </div>
                     <div>
-                        <span>New here? <Link to='/Signup'><ButtonSignIn /></Link> </span>
+                        {/* <span>New here? <Link to='/Signup'><ButtonSignIn /></Link> </span> */}
                     </div>
                 </form>
             </div >
--- client/src/pages/layout/RootLayout.jsx
+++ client/src/pages/layout/RootLayout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import ProtectionProvider from "../../context/auth/ProtectionProvider";
+import { useProfileStore } from "../../context/data/dataStore";
 
 export default function RootLayout() {
+
+  const {isOnline} = useProfileStore(state => state.defaultProfile)
   return (
     <div>
       RootLayout
-      <ProtectionProvider>
+      <ProtectionProvider isAllowed={isOnline}>
         <Outlet />
       </ProtectionProvider>
     </div>
